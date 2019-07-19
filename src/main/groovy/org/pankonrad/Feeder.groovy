cfbd3e3590cda41c2b09db0fa51ac3c353ab310b
pankonrad <grabk@o2.pl>
2019-07-19 11:36:03 +0200

    sdf

--- src/main/groovy/org/pankonrad/Feeder.groovy
+++ src/main/groovy/org/pankonrad/Feeder.groovy
@@ -5,25 +5,30 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 import io.micronaut.http.MediaType
+import javax.inject.Inject
 
 @CompileStatic
-@Controller("/feeder") 
+@Controller("/") 
 class Feeder {
 
-	final String preInitCmd = """echo 16 > /sys/class/gpio/unexport && echo 20 > /sys/class/gpio/unexport && echo 21 > /sys/class/gpio/unexport"""
-	final String initCmd = """echo 16 > /sys/class/gpio/export && echo 20 > /sys/class/gpio/export && echo 21 > /sys/class/gpio/export && echo out > /sys/class/gpio/gpio16/direction && echo out > /sys/class/gpio/gpio20/direction && echo out > /sys/class/gpio/gpio21/direction && echo 1 > /sys/class/gpio/gpio16/value && echo 0 > /sys/class/gpio/gpio20/value && echo 0 > /sys/class/gpio/gpio21/value"""
+    Gpio gpio
+  
+    @Inject
+    Feeder(Gpio gpio) {
+      this.gpio = gpio
+    }
 	
-	final String feederCmd1 = "echo 1 > /sys/class/gpio/gpio21/value"
-	final String feederCmd2 = "sleep 3"
-	final String feederCmd3 = "echo 0 > /sys/class/gpio/gpio21/value"
-	
-	
-	private Gpio gpio = new Gpio()
-
-    @Get("/")
+    @Get("/feeder/{state}/{milliseconds}")
     @Produces(MediaType.TEXT_PLAIN)
-    String run() {
-		gpio.run()
-		return "Done"
+    String run(int state, int milliseconds) {
+		gpio.run(state, milliseconds)
+		return "FEEDER OK"
+    }
+	
+	@Get("/pwm/{state}/{rate}/{range}")
+    @Produces(MediaType.TEXT_PLAIN)
+    String run(int state, int rate, int range) {
+		gpio.runPwm(state, rate, range)
+		return "PWM PIN OK"
     }
 }
