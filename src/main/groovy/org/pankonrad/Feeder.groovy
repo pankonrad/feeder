baf13156c684fba7f00f7bb64b9707b3a01f457c
pankonrad <grabk@o2.pl>
2019-08-19 08:05:16 +0200

    light and filter support

--- src/main/groovy/org/pankonrad/Feeder.groovy
+++ src/main/groovy/org/pankonrad/Feeder.groovy
@@ -12,23 +12,43 @@ import javax.inject.Inject
 class Feeder {
 
     Gpio gpio
+	SwitchBoxClient switchBoxClient
   
     @Inject
-    Feeder(Gpio gpio) {
+    Feeder(Gpio gpio, SwitchBoxClient switchBoxClient) {
       this.gpio = gpio
+	  this.switchBoxClient = switchBoxClient
     }
 	
     @Get("/feeder/{state}/{milliseconds}")
     @Produces(MediaType.TEXT_PLAIN)
     String run(int state, int milliseconds) {
+		milliseconds = (milliseconds > 1000) ? 1000 : milliseconds;
+		
+//		changeFilterState(0)
+		
 		gpio.run(state, milliseconds)
-		return "FEEDER OK"
+		String retVal = "FEEDER OK, state = " + state + ", milliseconds = " + milliseconds;
     }
 	
+	@Get("/light/{state}")
+    @Produces(MediaType.TEXT_PLAIN)
+	String changeLightState(int state) {
+		return switchBoxClient.light(state)
+	}
+	
+	@Get("/filter/{state}")
+    @Produces(MediaType.TEXT_PLAIN)
+	String changeFilterState(int state) {
+		return switchBoxClient.filter(state)
+	}
+	
+/*
 	@Get("/pwm/{state}/{rate}/{range}")
     @Produces(MediaType.TEXT_PLAIN)
     String run(int state, int rate, int range) {
 		gpio.runPwm(state, rate, range)
 		return "PWM PIN OK"
     }
+*/
 }
